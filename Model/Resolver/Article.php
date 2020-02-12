37727575685972a016deafc3f35a2cc017329c24
Eric <thainc@mageplaza>
2020-02-12 21:38:23 +0700

    Update GraphQl (doing)

---
+++ Model/Resolver/Article.php
@@ -0,0 +1,173 @@
+<?php
+/**
+ * Mageplaza
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Mageplaza.com license that is
+ * available through the world-wide-web at this URL:
+ * https://www.mageplaza.com/LICENSE.txt
+ *
+ * DISCLAIMER
+ *
+ * Do not edit or add to this file if you wish to upgrade this extension to newer
+ * version in the future.
+ *
+ * @category    Mageplaza
+ * @package     Mageplaza_FaqsGraphQl
+ * @copyright   Copyright (c) Mageplaza (https://www.mageplaza.com/)
+ * @license     https://www.mageplaza.com/LICENSE.txt
+ */
+
+declare(strict_types=1);
+
+namespace Mageplaza\FaqsGraphQl\Model\Resolver;
+
+use Magento\Framework\Exception\NoSuchEntityException;
+use Magento\Framework\GraphQl\Config\Element\Field;
+use Magento\Framework\GraphQl\Exception\GraphQlInputException;
+use Magento\Framework\GraphQl\Query\Resolver\Argument\SearchCriteria\Builder as SearchCriteriaBuilder;
+use Magento\Framework\GraphQl\Query\ResolverInterface;
+use Magento\Framework\GraphQl\Schema\Type\ResolveInfo;
+use Mageplaza\Faqs\Helper\Data;
+use Mageplaza\Faqs\Model\ResourceModel\Article\Collection;
+use Mageplaza\FaqsGraphQl\Model\Resolver\Filter\Query\Filter;
+
+/**
+ * Class Article
+ * @package Mageplaza\FaqsGraphQl\Model\Resolver
+ */
+class Article implements ResolverInterface
+{
+
+    /**
+     * @var Data
+     */
+    private $_helperData;
+
+    /**
+     * @var SearchCriteriaBuilder
+     */
+    private $searchCriteriaBuilder;
+
+    /**
+     * @var Filter
+     */
+    protected $filterQuery;
+
+    /**
+     * Categories constructor.
+     *
+     * @param Data $helperData
+     * @param SearchCriteriaBuilder $searchCriteriaBuilder
+     * @param Filter $filterQuery
+     */
+    public function __construct(
+        Data $helperData,
+        SearchCriteriaBuilder $searchCriteriaBuilder,
+        Filter $filterQuery
+    ) {
+        $this->_helperData           = $helperData;
+        $this->searchCriteriaBuilder = $searchCriteriaBuilder;
+        $this->filterQuery           = $filterQuery;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function resolve(Field $field, $context, ResolveInfo $info, array $value = null, array $args = null)
+    {
+        $this->validateArgs($args);
+        $searchCriteria = $this->searchCriteriaBuilder->build('article', $args);
+        $searchCriteria->setCurrentPage($args['currentPage']);
+        $searchCriteria->setPageSize($args['pageSize']);
+
+        switch ($args['action']) {
+            case 'get_all':
+                $collection = $this->_helperData->getArticleCollection();
+                break;
+            case 'get_with_visibility':
+                $collection = $this->_helperData->getArticleApiCollection();
+                break;
+            default:
+                throw new GraphQlInputException(__('No find your function'));
+        }
+
+        $searchResult = $this->filterQuery->getResult($searchCriteria, 'article', $collection);
+
+        $pageInfo = $this->getPageInfo($searchResult, $searchCriteria, $args);
+
+        return [
+            'total_count' => $searchResult->getTotalCount(),
+            'items'       => $searchResult->getItemsSearchResult(),
+            'pageInfo'    => $pageInfo
+        ];
+    }
+
+    /**
+     * @param $searchResult
+     * @param $searchCriteria
+     * @param $args
+     *
+     * @return array
+     * @throws GraphQlInputException
+     */
+    public function getPageInfo($searchResult, $searchCriteria, $args): array
+    {
+        //possible division by 0
+        if ($searchCriteria->getPageSize()) {
+            $maxPages = ceil($searchResult->getTotalCount() / $searchCriteria->getPageSize());
+        } else {
+            $maxPages = 0;
+        }
+
+        $currentPage = $searchCriteria->getCurrentPage();
+        if ($searchCriteria->getCurrentPage() > $maxPages && $searchResult->getTotalCount() > 0) {
+            throw new GraphQlInputException(
+                __(
+                    'currentPage value %1 specified is greater than the %2 page(s) available.',
+                    [$currentPage, $maxPages]
+                )
+            );
+        }
+
+        return [
+            'pageSize'        => $args['pageSize'],
+            'currentPage'     => $args['currentPage'],
+            'hasNextPage'     => $currentPage < $maxPages,
+            'hasPreviousPage' => $currentPage > 1,
+            'startPage'       => 1,
+            'endPage'         => $maxPages,
+        ];
+    }
+
+    /**
+     * @param $args
+     * @return Collection
+     * @throws GraphQlInputException
+     * @throws NoSuchEntityException
+     */
+    protected function getArticleWithVisibility($args): Collection
+    {
+        if (!isset($args['visibility'])) {
+            throw new GraphQlInputException(__('visibility value is not null'));
+        }
+        return $this->_helperData->getArticleApiCollection()->addFieldToFilter('visibility',$args['visibility']);
+    }
+
+    /**
+     * @param array $args
+     *
+     * @throws GraphQlInputException
+     */
+    protected function validateArgs(array $args)
+    {
+        if (isset($args['currentPage']) && $args['currentPage'] < 1) {
+            throw new GraphQlInputException(__('currentPage value must be greater than 0.'));
+        }
+
+        if (isset($args['pageSize']) && $args['pageSize'] < 1) {
+            throw new GraphQlInputException(__('pageSize value must be greater than 0.'));
+        }
+    }
+}
