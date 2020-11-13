fde32c3ced59f4c8df182c2a4c8dd63276ff6dbb
Eric <thainc@mageplaza.com>
2020-11-13 14:40:20 +0700

    Update Feature - Add Api get Config - dev done

---
+++ Model/Resolver/Config.php
@@ -0,0 +1,63 @@
+<?php
+/**
+ * Mageplaza
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Mageplaza.com license that is
+ * available through the world-wide-web at this URL:
+ * https://www.mageplaza.com/LICENSE.txt
+ *
+ * DISCLAIMER
+ *
+ * Do not edit or add to this file if you wish to upgrade this extension to newer
+ * version in the future.
+ *
+ * @category    Mageplaza
+ * @package     Mageplaza_FaqsGraphQl
+ * @copyright   Copyright (c) Mageplaza (https://www.mageplaza.com/)
+ * @license     https://www.mageplaza.com/LICENSE.txt
+ */
+
+declare(strict_types=1);
+
+namespace Mageplaza\FaqsGraphQl\Model\Resolver;
+
+use Magento\Framework\GraphQl\Config\Element\Field;
+use Magento\Framework\GraphQl\Query\ResolverInterface;
+use Magento\Framework\GraphQl\Schema\Type\ResolveInfo;
+use Mageplaza\FaqsGraphQl\Helper\Data;
+
+/**
+ * Class Config
+ * @package Mageplaza\FaqsGraphQl\Model\Resolver
+ */
+class Config implements ResolverInterface
+{
+
+    /**
+     * @var Data
+     */
+    private $_helperData;
+
+    /**
+     * Categories constructor.
+     *
+     * @param Data $helperData
+     */
+    public function __construct(
+        Data $helperData
+    ) {
+        $this->_helperData = $helperData;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function resolve(Field $field, $context, ResolveInfo $info, array $value = null, array $args = null)
+    {
+        $storeId = isset($args['storeId'])?$args['storeId']:null;
+
+        return $this->_helperData->getConfigValue('faqs', $storeId);
+    }
+}
